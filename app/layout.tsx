7c43eef736c7949605f4cf205d80dd375f5c5d87
vandana <vandanavaswani01@gmail.com>
2025-02-14 22:59:29 +0530

    Fixed fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Head } from "next/document";
 
 
 
@@ -27,6 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Give+You+Glory&family=Inter:wght@100..900&family=Manrope:wght@200..800&family=Mynerve&family=Orbitron:wght@400..900&family=Poppins:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`} 
