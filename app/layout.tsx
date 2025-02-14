0190ef718ead16d76964ad6af208bfc6b657fbc2
vandana <vandanavaswani01@gmail.com>
2025-02-14 22:40:44 +0530

    cursor changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "@/app/components/CustomCursor";
+
 
 
 
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`} 
       >
-        <CustomCursor />
+        
         {children}
       </body>
     </html>
